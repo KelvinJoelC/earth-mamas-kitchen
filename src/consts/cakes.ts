dfd69b0fca61b43b368b689e81407b84c4a09d1f
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-04-03 20:55:18 +1000

    Add Merienda font and update carousel component for improved styling; enhance cake props and adjust header visibility

--- src/consts/cakes.ts
+++ src/consts/cakes.ts
@@ -2,16 +2,28 @@ export const CAKES = [
     {
         id: 'mushroomLogCake',
         name: 'Mushroom Log Cake',
-        description: 'Vanilla flavoured ice cream with chunks of ch'
+        description: 'Vanilla flavoured ice cream with chunks of ch',
+        props: {
+            h: '350',
+            top: '0'
+        }
     },
     {
         id: 'mermaidCake',
         name: 'Mermaid Cake',
-        description: 'Vanilla flavoured ice cream with chunks of ch'
+        description: 'Vanilla flavoured ice cream with chunks of chsaadasdVanilla flavoured ice cream with chunks of chsaadasd',
+        props: {
+            h: '350',
+            top: '0'
+        }
     },
     {
         id: 'floralCake',
         name: 'Floral Cake',
-        description: 'Vanilla flavoured ice cream with chunks of ch'
+        description: 'Vanilla flavoured ice cream with chunks of chsaadasdVanilla flavoured ice cream with chunks of chsaadasdVanilla flavoured ice cream with chunks of chsaadasd',
+        props: {
+            h: '400',
+            padding: '20px'
+        }
     },
 ];
