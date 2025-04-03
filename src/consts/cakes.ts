d40b812346496997734a3154ad0adf65d32685e9
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-04-03 18:30:37 +1000

    Refactor Cakes section to use dynamic data from CAKES constant; update carousel component for improved maintainability

---
+++ src/consts/cakes.ts
@@ -0,0 +1,17 @@
+export const CAKES = [
+    {
+        id: 'mushroomLogCake',
+        name: 'Mushroom Log Cake',
+        description: 'Vanilla flavoured ice cream with chunks of ch'
+    },
+    {
+        id: 'mermaidCake',
+        name: 'Mermaid Cake',
+        description: 'Vanilla flavoured ice cream with chunks of ch'
+    },
+    {
+        id: 'floralCake',
+        name: 'Floral Cake',
+        description: 'Vanilla flavoured ice cream with chunks of ch'
+    },
+];
