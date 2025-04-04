3f95c66231d3947e293edef44b28dadef6e8e5d5
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-04-04 15:59:19 +1000

    Add Explore section and dynamic data for cake options; update Cakes section layout and styling

---
+++ src/consts/explore.ts
@@ -0,0 +1,29 @@
+export const EXPLORE = [
+    {
+        id:'bespokeCakes',
+        title:'Bespoke cakes',
+        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo aliquid vero fugit aliquam cum enim asperiores possimus nam deserunt, dolorum architecto,',
+        img:'',        
+        props:{
+            bg: 'red'
+        }
+    },
+    {
+        id:'bespokeCakes',
+        title:'Edible blooms',
+        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo aliquid vero fugit aliquam cum enim asperiores possimus nam deserunt, dolorum architecto,',
+        img:'',      
+        props:{
+            bg: 'blue'
+        }
+    },
+    {
+        id:'flowerCupcakesBouquet',
+        title:'Flower Cupcakes Bouquet',
+        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo aliquid vero fugit aliquam cum enim asperiores possimus nam deserunt, dolorum architecto,',
+        img:'',      
+        props:{
+            bg: 'green'
+        }
+    },
+]
