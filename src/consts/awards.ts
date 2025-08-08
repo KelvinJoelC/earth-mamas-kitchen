ecaf83bcb8855592e262195194cb3e9359343f4f
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-08-08 16:23:38 +0200

    Add awards data and create Awards section with carousel display
    - Introduced a new awards data file (awards.ts) containing award details for various events.
    - Developed an Awards section in Astro that utilizes the Embla Carousel for displaying awards.
    - Implemented a marquee effect for highlighting rewards.
    - Each award displays event name, position, title, and description with corresponding images.

---
+++ src/consts/awards.ts
@@ -0,0 +1,39 @@
+export const AWARDS = [
+  {
+    id: 'award',
+    event: '2024 Cairns Show',
+    position: '1st',
+    title: "Any Vegan cake",
+    description: "Vegan Chocolate sponge with raspberry jam and vanilla buttercream",
+    icon: "/icons/gold-medal.svg"
+  },
+  {
+    id: 'award',
+    position: '1st',
+    event: '2024 Cairns Show',
+    title: "Any cake not mentioned",
+    description: "Chai spiced carrot cake with chai cream cheese icing",
+    icon: "/icons/leaf-award.svg"
+  },
+  {
+    id: 'award',
+    position: '2st',
+    event: '2024 Cairns Show',
+    title: "Decorated cupcakes",
+    description: "Sunflower chai carrot cupcakes with vanilla buttercream",
+  },
+  {
+    id: 'award',
+    position: '1st',
+    event: '2019 Cairns Show',
+    title: "Muffins - Any variety",
+    description: "Flavour: strawberry, white chocolate, cinnamon and macadamia",
+  },
+  {
+    id: 'award',
+    position: '1st',
+    event: '2019 Cairns Show',
+    title: "Any cake not mentioned",
+    description: "Hummingbird cake",
+  },
+];
